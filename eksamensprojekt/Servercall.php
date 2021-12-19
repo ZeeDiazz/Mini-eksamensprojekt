5fb0bff659bd22c60b7c10a05b2c5d948983bc5e
Daniel9915 <69625109+Daniel9915@users.noreply.github.com>
2021-12-19 21:34:13 +0100

    Delete Servercall.php

--- eksamensprojekt/Servercall.php
+++
@@ -1,9 +0,0 @@
-<?php
-    //mysql_connect("localhost","root","");
-    //mysql_select_db("login");
-    $conn = new mysqli("mini-eksamensprojekt","root","");
-    if($conn-> connect_error){
-        die("error: " . $conn->connect_error);
-    }
-
-?>
